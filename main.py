9bb75282500d0c722f2626e4e921bb32700377df
phigo <philipp.golda@googlemail.com>
2024-11-15 21:21:35 +0000

    Contracts changed based on current deployment

--- main.py
+++ main.py
@@ -23,12 +23,12 @@ yf_tickers = ["AAPL", "TSLA", "MSTR", "XAGX-USD", "NVDA"]
 data: dict = {ticker: {"dex_price": float(0), "yf_price": float(0), "premium": float(0), "last_premium": float(0), "time": 0, "limit": [0]} for ticker in tickers}
 
 assets = Assets()
-assets.add_asset("USD", "0xA2efBedf2a9954aC0854933d148F9B5Df9C1FeE0", "")
-assets.add_asset("AAPL", "0x5978a4fEe819ddbf2195a3671e73D18F9Dae7ae7", "0xe9BF367408567CdE44ca83548e596748e997b2EB")
-assets.add_asset("TSLA", "0x895D225434b4c889eAd8Bc30aFF8c770Eefc177D", "0x73d8739b61A7BFD613d96aB8A6EDd03394D2f965")
-assets.add_asset("MSTR", "0x10D2c0522D5852Ba2eEc51542036bEC7E01b418a", "0x58fdbf9416F385E2Fa737ab1059bD3940b7443bD")
-assets.add_asset("XAG", "0xA8Cc0b87B8f91Fa4070F67a89bc2DB3ab4c94F62", "0x1b620Da7d7c63b9D2bCB1549BEd1f4aae542d85B")
-assets.add_asset("NVDA", "0x0E4a80556B98fF991D6700BBe5A99f51D224623d", "0x0eA63793856fDeE52F9EB49547BD39724b4E9560")
+assets.add_asset("USD", "0xAE34739a521521DE17902999ff8FBb12394192a1", "")
+assets.add_asset("AAPL", "0x03991Ef0b2987487B72829b793e418F8757DE376", "0xdcaCA0215344c3fc3DAB35e0432319fBF53d53ab")
+assets.add_asset("TSLA", "0x2E5AE8fbE2e5cd9dA8710E1DDebe64A1BA121Ae4", "0x2b2c69a9f5677f4169b0A464CB36B9D744c03a81")
+assets.add_asset("MSTR", "0x284352a2E970C7B83Ae069A8de7dd61F4e7f5f7a", "0xd8Bd5F7acAB978F36c7ae5AF9ebf1388EE9334C9")
+assets.add_asset("XAG", "0xc01b65f7B86a3D171b8C7f1CE3B437Bd4Ca68093", "0x95218d7672f80Ac345c06BbE64Bf0657744e74b8")
+assets.add_asset("NVDA", "0x7938486225755BeC1fc202CA80799d27f108B33f", "0xFfa2FCf91033E013BAb79548a5B3F151023adEA3")
 
 yf = YFinance()
 yf.start()
@@ -87,48 +87,52 @@ def update_dex_premium():
         except:
             data[name]["premium"] = 100.0
 def run_bot():
-    t0=time.time()
-    while True:
-        if all_prices_updated():
-            break
-        else:
-            print(data)
-            time.sleep(1)
-    bot.send_telegram_msg('Setup was successfull. Wait 5s to iterate trough all data.')
-    time.sleep(5)
-    update_dex_premium()
-    send_update()
-    while True:
+    try:
+        t0=time.time()
+        while True:
+            if all_prices_updated():
+                break
+            else:
+                print(data)
+                time.sleep(1)
+        bot.send_telegram_msg('Setup was successfull. Wait 5s to iterate trough all data.')
+        time.sleep(5)
         update_dex_premium()
-        if time.time()-t0 >=60*60:
-            send_update()
-            t0 = time.time()
-        for name in data:
-            premium_value = data[name]["premium"]
-            #print(premium_value)
-            if abs(premium_value - 100) > 10.0 and premium_value != 0:
-                if time.time() - data[name]["time"] > 60*5:
-                    amountin = calc_volume(name)
-                    data[name]["time"] = time.time()
-                    message = "Asset, Oracle/Dex, Premium\n"
-                    message += f"{name}, {data[name]['dex_price']:.2f} jUSD / {data[name]['yf_price']:.2f} $, {data[name]['premium']:.2f} %\n"
-                    if premium_value - 100 < 0.0:
-                        message += f"Swappe {amountin:.2f} jUSD gegen {name} damit Ratio bei 90 %\n"
+        send_update()
+        while True:
+            update_dex_premium()
+            if time.time()-t0 >=60*60:
+                send_update()
+                t0 = time.time()
+            for name in data:
+                premium_value = data[name]["premium"]
+                #print(premium_value)
+                if abs(premium_value - 100) > 10.0 and premium_value != 0:
+                    if time.time() - data[name]["time"] > 60*30:
+                        amountin = calc_volume(name)
+                        data[name]["time"] = time.time()
+                        message = "Asset, Oracle/Dex, Premium\n"
+                        message += f"{name}, {data[name]['dex_price']:.2f} jUSD / {data[name]['yf_price']:.2f} $, {data[name]['premium']:.2f} %\n"
+                        if premium_value - 100 < 0.0:
+                            message += f"Swappe {amountin:.2f} jUSD gegen {name} damit Ratio bei 90 %\n"
+                            bot.send_telegram_msg(message)
+                        if premium_value - 100 > 0.0:
+                            message += f"Swappe {amountin:.2f} {name} gegen jUSD damit Ratio bei 110 %\n"
+                            bot.send_telegram_msg(message)
+            for name in data:
+                if abs(data[name]["premium"] - 100) < 10.0 and abs(data[name]["last_premium"] - 100) > 10 or abs(
+                        data[name]["premium"] - 100) > 10.0 and abs(data[name]["last_premium"] - 100) < 10:
+                    if (data[name]["premium"] != 0 and data[name]["last_premium"] != 0):
+                        message = f"Premium Changed for {name}\n"
+                        message += f"Old: {data[name]['last_premium']:.2f} %, New: {data[name]['premium']:.2f} %\n"
                         bot.send_telegram_msg(message)
-                    if premium_value - 100 > 0.0:
-                        message += f"Swappe {amountin:.2f} {name} gegen jUSD damit Ratio bei 110 %\n"
-                        bot.send_telegram_msg(message)
-        for name in data:
-            if abs(data[name]["premium"] - 100) < 10.0 and abs(data[name]["last_premium"] - 100) > 10 or abs(
-                    data[name]["premium"] - 100) > 10.0 and abs(data[name]["last_premium"] - 100) < 10:
-                if (data[name]["premium"] != 0 and data[name]["last_premium"] != 0):
-                    message = f"Premium Changed for {name}\n"
-                    message += f"Old: {data[name]['last_premium']:.2f} %, New: {data[name]['premium']:.2f} %\n"
-                    bot.send_telegram_msg(message)
-        for name in data:
-            data[name]["last_premium"] = data[name]["premium"]
+            for name in data:
+                data[name]["last_premium"] = data[name]["premium"]
 
-        time.sleep(1)
+            time.sleep(1)
+    except Exception as e:
+        pass
+        print(e)
 
 if __name__ == '__main__':
     # Start der Preisaktualisierung in einem separaten Thread
